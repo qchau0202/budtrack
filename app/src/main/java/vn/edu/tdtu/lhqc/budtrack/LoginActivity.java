fea10e53d348d8470418fdfc1de905494654ab2c
iamchouty <90039986+qchau0202@users.noreply.github.com>
2025-11-03 17:32:51 +0700

    ADDED: [Customized Main activity][Added illustrations, navigation to Login page]

--- app/src/main/java/vn/edu/tdtu/lhqc/budtrack/LoginActivity.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/budtrack/LoginActivity.java
@@ -1,9 +1,6 @@
 package vn.edu.tdtu.lhqc.budtrack;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
     }
 }
