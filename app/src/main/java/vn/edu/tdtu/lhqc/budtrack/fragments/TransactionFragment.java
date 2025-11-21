e2cdf43a9cb4494d95df3d7cc465104f25b37e69
ngthtnhung <ngthtnhung2005@gmail.com>
2025-11-21 16:57:33 +0700

    Edit transaction screen

--- app/src/main/java/vn/edu/tdtu/lhqc/budtrack/fragments/TransactionFragment.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/budtrack/fragments/TransactionFragment.java
@@ -1,7 +1,6 @@
 package vn.edu.tdtu.lhqc.budtrack.fragments;
 
 import android.app.DatePickerDialog;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
